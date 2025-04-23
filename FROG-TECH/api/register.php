538eb0720029ca31211975792e3957ea4c9b13fe
FeJoestar18 <amarofelipe504@gmail.com>
2025-04-23 11:51:01 -0300

    Senha e cpf desvinculados

--- FROG-TECH/api/register.php
+++ FROG-TECH/api/register.php
@@ -8,6 +8,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $cpf = $_POST['cpf'];
     $telefone = $_POST['telefone'];
 
+    // Validações
     if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
         echo "E-mail inválido!";
         exit;
@@ -23,7 +24,21 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         exit;
     }
 
-    $senhaHash = password_hash($senha, PASSWORD_DEFAULT);
+    if (!preg_match('/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W_]).{8,}$/', $senha)) {
+        echo "A senha deve ter no mínimo 8 caracteres, incluindo letra maiúscula, minúscula, número e caractere especial.";
+        exit;
+    }
+   
+        $senhaHash = password_hash($senha, PASSWORD_DEFAULT);
+        
+    $verificaCpf = $pdo->prepare("SELECT id FROM usuarios WHERE cpf = :cpf");
+    $verificaCpf->bindParam(':cpf', $cpf);
+    $verificaCpf->execute();
+
+    if ($verificaCpf->rowCount() > 0) {
+        echo "Este CPF já está cadastrado!";
+        exit;
+    }
 
     $sql = "INSERT INTO usuarios (email, senha, cpf, telefone) VALUES (:email, :senha, :cpf, :telefone)";
     $stmt = $pdo->prepare($sql);
