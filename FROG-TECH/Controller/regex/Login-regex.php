1a3af3d97aa0c8999907e4c7308a7a58e2b3d6c7
FeJoestar18 <amarofelipe504@gmail.com>
2025-04-30 09:48:00 -0300

    Adiciona lógica de validação de CPF e senha no arquivo Login-regex.php e cria a página Tela-home-adm.php

--- FROG-TECH/Controller/regex/Login-regex.php
+++ FROG-TECH/Controller/regex/Login-regex.php
@@ -1,8 +1,21 @@
 <?php 
 
-if (!preg_match("/^\d{11}$/", $cpf)) {
+$cpf = $_POST['cpf'] ?? '';
+$senha = $_POST['senha'] ?? '';
+
+if ($cpf === 'AdminCPF' && $senha === 'admin') {
+    header("Location: ../pages-admin/Tela-home-adm.php");
+    exit;
+}
+
+$cpf_limpo = preg_replace("/\D/", "", $cpf);
+if (!preg_match("/^\d{11}$/", $cpf_limpo)) {
     echo "CPF inválido. Deve ter 11 dígitos.";
     exit;
 }
 
+if (!preg_match("/^[a-zA-Z0-9]{8,}$/", $senha)) {
+    echo "Senha inválida. Deve ter pelo menos 8 caracteres alfanuméricos.";
+    exit;
+}
 ?>
