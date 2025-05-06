78dc242bf64082db6afd9f9365c6f5f20997adbf
FeJoestar18 <amarofelipe504@gmail.com>
2025-05-06 01:14:26 -0300

    Refatora lógica de login para redirecionamento baseado em função de usuário e garante inicialização da sessão.

--- FROG-TECH/Controller/auth-login.php
+++ FROG-TECH/Controller/auth-login.php
@@ -1,8 +1,9 @@
 <?php
-session_start(); 
+
 include_once('Conect/config-url.php');
 include ('../Controller/regex/Login-regex.php');
 include('../Controller/Conect/conecao.php'); 
+session_start(); 
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $cpf = $_POST['cpf'];
@@ -17,10 +18,23 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
     if ($user) {
         if (password_verify($senha, $user['senha'])) {
-           
             $_SESSION['user_id'] = $user['id'];
             $_SESSION['user_email'] = $user['email']; 
-            header("Location: " . BASE_URL . "/pages-usuario/Tela-home-usuario.php");
+
+            switch ($user['role_id']) {
+                case 1:
+                    header("Location: " . BASE_URL . "pages-admin/Tela-home-adm.php");
+                    break;
+                case 2:
+                    header("Location: " . BASE_URL . "pages-funcionario/Tela-home-func.php");
+                    break;
+                case 3:
+                    header("Location: " . BASE_URL . "pages-usuario/Tela-home-usuario.php");
+                    break;
+                default:
+                    echo "Função de usuário inválida.";
+                    break;
+            }
             exit; 
         } else {
             echo "Senha incorreta!";
