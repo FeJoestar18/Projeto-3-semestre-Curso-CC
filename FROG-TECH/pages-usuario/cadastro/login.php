da7192346bda32e536d3216f6e98f0231c8956a4
FeJoestar18 <amarofelipe504@gmail.com>
2025-05-12 14:31:58 -0300

    Remove estilos desnecessários da página de login e ajusta a estrutura do CSS

--- FROG-TECH/pages-usuario/cadastro/login.php
+++ FROG-TECH/pages-usuario/cadastro/login.php
@@ -14,20 +14,22 @@ include_once('../../Controller/Conect/config-url.php');
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
     <link rel="stylesheet" href="<?= BASE_URL ?>css/modalERROR.css">
-    <link rel="stylesheet" href="<?= BASE_URL ?>/css/layouts/rodape.css">
+    <link rel="stylesheet" href="<?= BASE_URL ?>/css/layouts/rodape-cadastro.css">
     <link rel="stylesheet" href="<?= BASE_URL ?>css/css-usuarios/pageLogin.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
 </head>
 <body>
-    <div class ="back-button-container">
+
+<!-- BOTÃO BUGADO -->
+    <!-- <div class ="back-button-container">
         <a href="javascript:history.back()" class="back-button">
                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" stroke="#d32f2f" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-arrow-left">
                     <line x1="19" y1="12" x2="5" y2="12"></line>
                 <polyline points="12 19 5 12 12 5"></polyline>
             </svg>
          </a>
-    </div>
-
+    </div> -->
+    
         <header class="header">
             <img src="<?= BASE_URL ?>img/logo/Logo-Frog-v1.png" alt="FrogTech Logo" class="logo">
         </header>
@@ -66,7 +68,25 @@ include_once('../../Controller/Conect/config-url.php');
         }
     </script>
         <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+<style> 
+.login-container {
+            background-color: white;
+            border-radius: 25px;
+            padding: 30px;
+            display: flex;
+            align-items: center;
+            justify-content: space-between;
+            transform: translateX(-25%);
+            box-shadow: rgba(0, 0, 0, 0.25) 0px 54px 55px, rgba(0, 0, 0, 0.12) 0px -12px 30px, rgba(0, 0, 0, 0.12) 0px 4px 6px, rgba(0, 0, 0, 0.17) 0px 12px 13px, rgba(0, 0, 0, 0.09) 0px -3px 5px;
+            transition: transform 0.3s ease;
 
+        }
+                .logo {
+                max-width: 200px;
+                margin-bottom: 600px;
+                margin-left: 100%;
+            }
+</style>
 <?php
 session_start();
 if (isset($_SESSION['error'])) {
